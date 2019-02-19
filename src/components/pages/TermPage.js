d8daabd86cb9ac10413f463e942114901dade7c1
Alexandr Yakovis <yakovia@TDS-6S60K2.fdny.org>
2019-02-19 12:20:39 -0500

    events handling

--- src/components/pages/TermPage.js
+++ src/components/pages/TermPage.js
@@ -1,17 +1,43 @@
 import React, { Component } from "react";
 import Button from '@material-ui/core/Button';
 class TermPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // // This binding is necessary to make `this` work in the callback
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  // // binded handleClick
+  // handleClick() {
+  //   this.setState(state => ({
+  //     isToggleOn: !state.isToggleOn
+  //   }));
+  // }
+
+  handleClick = () => {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+ 
   render() {
     return (
       <div>
         <h2>Terminal</h2>
-        
-        <Button variant="contained" color="primary">
-          10-96
+        <hr/>
+        <Button variant="contained" color="primary"
+         onClick={this.handleClick}>
+         {this.state.isToggleOn ? 'ON' : 'OFF'}
         </Button>
+
       </div>
     );
   }
+
+ 
 }
  
 export default TermPage;
