9fb64fea0c87354565f4b56d6ed3fba04398a2c6
iMonstrik <coolspammail@gmail.com>
2019-02-27 18:31:43 -0500

    add notes redux

--- src/components/pages/TermPage.js
+++ src/components/pages/TermPage.js
@@ -1,13 +1,21 @@
 import React, {Component} from "react";
-import Button from '@material-ui/core/Button';
-import NickNameForm from "../forms/NickNameForm";
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from '../../todos/reducers'
+
+import VisibleTodoList from "../../todos/containers/VisibleTodoList";
+import AddTodo from "../../todos/containers/AddTodo";
+import Footer from "../../todos/components/Footer";
+
+
 
 class TermPage extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {isToggleOn: true};
-
+        // this.state = {isToggleOn: true};
+        this.store = createStore(rootReducer)
         // // This binding is necessary to make `this` work in the callback
         // this.handleClick = this.handleClick.bind(this);
     }
@@ -19,23 +27,28 @@ class TermPage extends Component {
     //   }));
     // }
 
-    handleClick = () => {
-        this.setState(state => ({
-            isToggleOn: !state.isToggleOn
-        }));
-    }
+    // handleClick = () => {
+    //     this.setState(state => ({
+    //         isToggleOn: !state.isToggleOn
+    //     }));
+    // }
 
     render() {
         return (
             <div>
-                <h2>Terminal</h2>
-                <hr/>
-                <Button variant="contained" color="primary" size="small"
-                        onClick={this.handleClick}>
-                    {this.state.isToggleOn ? 'ON' : 'OFF'}
-                </Button>
-                <hr/>
-                <NickNameForm/>
+                <Provider store={this.store}>
+                    <AddTodo />
+                    <VisibleTodoList />
+                    <Footer />
+                </Provider>,
+                {/*<h2>Terminal</h2>*/}
+                {/*<hr/>*/}
+                {/*<Button variant="contained" color="primary" size="small"*/}
+                        {/*onClick={this.handleClick}>*/}
+                    {/*{this.state.isToggleOn ? 'ON' : 'OFF'}*/}
+                {/*</Button>*/}
+                {/*<hr/>*/}
+                {/*<NickNameForm/>*/}
 
             </div>
         );
